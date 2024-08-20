137c58efc121abc4de79da586fb05993a4ee4349
kodunaiff <kodunaiff@gmail.com>
2024-08-20 21:01:39 +0800

    добавил функционал с тегами

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,9 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from environs import Env
+from taggit.models import Tag
+from django.db.models import Count
+
 
 env = Env()
 env.read_env()
@@ -22,8 +25,12 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -39,7 +46,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -55,11 +63,17 @@ def post_detail(request, year, month, day, post):
     # Форма для комментирования пользователями
     form = CommentForm()
 
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
