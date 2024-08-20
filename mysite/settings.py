4673e12ec25f190eb1f0f0f61b7759b20a581cdc
kodunaiff <kodunaiff@gmail.com>
2024-08-20 22:10:58 +0800

    добавил фильтры, карты

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
